13f90bc091b0b91ffc43829cbfb73f2a34077866
yuhongjun <yuhj3@asiainfo.com>
2021-06-30 17:38:14 +0800

    function closure

--- Python3_study/_test/FunctionStudy.py
+++ Python3_study/_test/FunctionStudy.py
@@ -103,3 +103,63 @@ def func():
 
 
 func()
+
+
+print('--------------------函数对象--------------------')
+
+
+def add(x, y):
+    return x + y
+
+
+# 1、函数赋值给一个引用
+func = add
+print(f'{func(1,2)}')
+
+# 2、函数作为容器元素
+map = {'a': add, 'b': max}
+print(map)
+print(map['a'](1, 2))
+print(map.get('a')(1, 2))
+
+# 3、函数作为函数的参数
+
+
+def foo(x, y, paramater):
+    return x + y + paramater(x, y)
+
+
+print(foo(1, 2, func))
+
+# 4、函数作为函数的返回值
+
+
+def bar():
+    return add
+
+
+func = bar()
+func(1, 2)
+
+
+print('--------------------函数闭包closures--------------------')
+'''
+闭包，又称闭包函数或者闭合函数，其实和前面讲的嵌套函数类似，不同之处在于，闭包中外部函数返回的不是一个具体的值，而是一个函数。
+一般情况下，返回的函数会赋值给一个变量，这个变量可以在后面被继续执行调用。
+'''
+
+#闭包函数，其中 exponent 称为自由变量
+def nth_power(exponent):
+    x = 'x'
+    y='y'
+    def exponent_of(base):
+        x,y
+        return base ** exponent
+    return exponent_of # 返回值是 exponent_of 函数
+square = nth_power(2) # 计算一个数的平方
+cube = nth_power(3) # 计算一个数的立方
+print(f'计算 2 的平方:{square(2)},{nth_power(2)(2)}') # 计算 2 的平方
+print(f'计算 2 的立方:{cube(2)},{nth_power(3)(2)}') # 计算 2 的立方
+print(nth_power.__closure__)
+print(square.__closure__)
+print(square.__closure__[0].cell_contents,square.__closure__[1].cell_contents,square.__closure__[2].cell_contents)
