087f927b8d5a32491c1ff76c8c63cf40a672283f
yuhongjun <yuhj3@asiainfo.com>
2021-07-15 22:39:21 +0800

    decorator v2

--- Python3_study/_test/decorator_study.py
+++ Python3_study/_test/decorator_study.py
@@ -28,3 +28,30 @@ a_function_requiring_decoration()
 print(a_function_requiring_decoration.__name__)
 print(a_function_requiring_decoration.__closure__)
 print(a_function_requiring_decoration.__closure__[0].cell_contents)
+
+
+# 标准装饰器模板
+def decorator_name(func):
+    from functools import wraps
+
+    @wraps(func)
+    # 此注解作用是将代理的方法模拟成被装饰的方法，一些基本参数与被装饰的方法一致
+    # 若不加此注解，那么在调用func.__name__时会返回decorated
+    def decorated(*args, **kwargs):
+        if not can_run:
+            return "function can not run."
+        return func(*args, **kwargs)
+    return decorated
+
+
+@decorator_name  # func = decorator_name(func)
+def func():
+    return("function can run")
+
+
+can_run = True
+print(func())
+
+can_run = False
+print(func())
+print(func.__name__)
--- Python3_study/_test/file_study.py
+++ Python3_study/_test/file_study.py
@@ -1,5 +1,33 @@
 # coding:'utf-8'
 # open返回的对象只能在with代码块内部使用，但with代码块内的变量，可以直接在代码块外部使用
-with open('class_study.py',mode='rt',encoding='utf-8') as f:
+with open('class_study.py', mode='rt', encoding='utf-8') as f:
     line = f.readline()
+print(line)
+
+# f.seek(n,模式):n指的是移动的字节个数
+# 模式：
+# 模式0：参照物是文件开头位置
+# f.seek(9,0)
+# f.seek(3,0) # 指针位置3
+
+# 模式1：参照物是当前指针所在位置
+# f.seek(9,1)
+# f.seek(3,1) # 指针位置12
+
+# 模式2：参照物是文件末尾位置，应该倒着移动
+# f.seek(-9,2) # 3
+# f.seek(-3,2) # 9
+
+import json  # 不是所有格式都可以读取
+with open('file_study_content.json', mode='rt', encoding='utf-8') as f:
+    line = json.load(f)
+print(line)
+
+
+try:
+    with open(r'd:\file_study_content.json', mode='rt', encoding='utf-8') as f:
+        line = json.load(f)
+except:
+    with open(r'd:\file_study_content.json', mode='wt', encoding='utf-8') as f:
+        json.dump(line, f)
 print(line)
